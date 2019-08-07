b4cc887898b0f04025b271c48ed5b5fc72561462
qq1415551519 <1415551519@qq.com>
2019-08-07 11:04:41 +0800

    add sign to database

---
+++ tp5/application/common/model/UserAutographModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 12155
+ * Date: 2019/8/7
+ * Time: 10:58
+ */
+
+namespace app\common\model;
+
+
+class UserAutographModel extends BaseModel
+{
+    protected $name = 'user_autograph';
+
+}
--- tp5/application/userapi/controller/v1/UserAutograph.php
+++ tp5/application/userapi/controller/v1/UserAutograph.php
@@ -10,6 +10,7 @@ namespace app\userapi\controller\v1;
 
 
 use app\common\exception\ParamException;
+use app\common\model\UserAutographModel;
 use app\userapi\controller\UserApi;
 use think\facade\Log;
 use think\Request;
@@ -25,7 +26,16 @@ class UserAutograph extends UserApi
     {
         $content = $request->param('content');
 
-        Log::record(222,'demo');
+        //  保存到数据库
+        $data = [
+            'user_id' => $request->user->id,
+            'add_time' => time(),
+            'create_time' => date('Y-m-d H:i:s'),
+        ];
+        UserAutographModel::create($data);
+        //  保存到数据库
+        Log::record('-----------------记录copy的内容---------------','demo');
+        Log::record($content,'demo');
         //  验证是否格式正确
 //        $content = '特3456书yuuo莞6543李zxcz蒜7782法fgnv级完2347全dfji试3726测asad感3847知qwez到';
 
