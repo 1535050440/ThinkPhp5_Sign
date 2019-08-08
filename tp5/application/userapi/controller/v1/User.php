504eb4639e225ce316beb38e9d8b65b8a6031ee6
qq1415551519 <1415551519@qq.com>
2019-08-08 08:16:00 +0800

    2019-08-08-

--- tp5/application/userapi/controller/v1/User.php
+++ tp5/application/userapi/controller/v1/User.php
@@ -9,8 +9,10 @@
 namespace app\userapi\controller\v1;
 
 
+use app\common\model\UserModel;
 use app\userapi\controller\UserApi;
 use think\facade\Log;
+use think\Request;
 
 class User extends UserApi
 {
@@ -18,6 +20,28 @@ class User extends UserApi
     {
         Log::record(222,'demo');
         echo 'show';
+    }
+
+    /**
+     * @param Request $request
+     * @deng      2019/8/8    8:12
+     */
+    public function updateInfo(Request $request)
+    {
+        $nick_name = $request->param('nick_name');
+        $sex = $request->param('sex')==1?1:2;
+        $avatar = $request->param('avatar');
+
+        $user_id = $request->user->id;
+
+        $userFind = UserModel::get($user_id);
+        $userFind->nick_name = base64_encode($nick_name);
+        $userFind->sex = $sex;
+        $userFind->avatar = $avatar;
+
+        $userFind->save();
+
+        $this->success($userFind);
     }
 
 }
